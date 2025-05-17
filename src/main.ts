5292518c8114409421036942313f4262a04cc447
captain-blue210 <captain.blue210@gmail.com>
2025-05-17 22:13:54 +0900

    feat: Add Kindle sync button to ribbon with download icon

--- src/main.ts
+++ src/main.ts
@@ -53,6 +53,11 @@ export default class KindleHighlightsPlugin extends Plugin {
 			DEFAULT_SETTINGS,
 			await this.loadData()
 		);
+
+		// Add ribbon icon at the end
+		this.addRibbonIcon("download", "Sync Kindle Highlights", () =>
+			this.syncHighlights()
+		);
 	}
 
 	async saveSettings() {
